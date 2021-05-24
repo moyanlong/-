5cf12279662537e393d3a624a9cb0fb02c0fd827
莫延龙 <1748439718@qq.com>
2021-05-24 09:56:21 +0800

    '莫主分支第一次提交'

---
+++ js/toubutiaozhuan.js
@@ -0,0 +1,29 @@
+// 跳转
+var  mtzbqz=document.getElementById('dingbu').firstElementChild.firstElementChild.children
+
+mtzbqz[1].onclick=function(){
+    mtzbqz[1].firstElementChild.href='./index.html'
+}
+mtzbqz[2].onclick=function(){
+    mtzbqz[2].firstElementChild.href='./faxiankuwan.html'
+}
+mtzbqz[3].onclick=function(){
+    mtzbqz[3].firstElementChild.href='./daogou.html'
+}
+mtzbqz[4].onclick=function(){
+    mtzbqz[4].firstElementChild.href='./7.html'
+}
+mtzbqz[5].onclick=function(){
+    mtzbqz[5].firstElementChild.href='./体验报告-最新.html'
+}
+mtzbqz[7].onclick=function(){
+    mtzbqz[7].firstElementChild.href='./regist.html'
+}
+mtzbqz[6].onmouseover=function(){
+    mtzbqz[6].firstElementChild.style.width='28px'
+    mtzbqz[6].firstElementChild.style.height='28px'
+}
+mtzbqz[6].onmouseout=function(){
+    mtzbqz[6].firstElementChild.style.width='25px'
+    mtzbqz[6].firstElementChild.style.height='25px'
+}
